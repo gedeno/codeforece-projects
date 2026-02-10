b810fadb2c4ad3e037ef14c7284d35b3d2cb4f05
gedeno <gedenogeremew@gmail.com>
2026-02-10 13:49:20 +0300

    anathor problem is solved

---
+++ boy&andgirl.py
@@ -0,0 +1,15 @@
+x = 'xiaodao'
+lens = len(x)
+v = []
+for i in x:
+    if i in v:
+        pass
+    else:
+        v.append(i)
+
+gen = len(v)
+gender = gen%2
+if gender == 0:
+    print('CHAT WITH HER!')
+else:
+    print('IGNORE HIM!')
