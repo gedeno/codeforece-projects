0dad55ee8535595809e6ce9419299dd2eeb47ec5
gedeno <gedenogeremew@gmail.com>
2026-02-14 16:54:23 +0300

    new problem is solved

---
+++ stodecolor.py
@@ -0,0 +1,13 @@
+stone = str(input('Enter your stode: '))
+instrunct = str(input('Enter your instrunct: '))
+X = len(stone)
+v = 0
+for j in instrunct:
+    if stone[v] == j :
+        v = v + 1
+print(v+1)
+
+
+
+
+
